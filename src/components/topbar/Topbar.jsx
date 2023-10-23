2278d1da6bcb647bee78fe33ac141afa7605a4ee
Jaaliyah <jaaliyah_alexander@outlook.com>
2023-10-22 16:26:04 -0800

    Some changes made to login and home

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,19 +1,17 @@
 import './topbar.css';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Topbar = () => {
     return (
-        <header className="topbar-sticky">
-            <nav className="topbar">
-                <div className="topbar-logo">
-                    <span className="logo">Alaska Faith Community Nurse Rescource Center</span>
-                </div>
-                <div class="topbar-loginlink">
-                    <Link to="login"> Login </Link>
-                </div>
+        <nav className="navbar sticky-top bg-primary">
+            <div className="container-fluid">
+                <span className="navbar-brand text-white">Alaska Faith Community Nurse Rescource Center</span>
+                <NavLink to="/login"
+                    className={({ isActive }) => (isActive ? 'nav-link disabled' : 'navbar-text d-flex')}> Login </NavLink>
+            </div>
 
-            </nav>
-        </header>
+
+        </nav >
     )
 }
 
---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+const Dashboard = () => {
+    return (
+        <Sidebar />
+    )
+}
+
+export default Dashboard;
